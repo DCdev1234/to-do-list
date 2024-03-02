2b34ea1790466ae7417d718aec57d47417ceb408
declan <declan9804@gmail.com>
2024-03-02 02:20:32 +0000

    Added ability to add item

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ export type ItemType = {
 
 export default function Home() {
   const [items, setItems] = useState<ItemType[]>([])
+  const [inputField, setInputField] = useState<string>("")
 
   useEffect(()=>{
     console.log("loaded")
@@ -25,11 +26,27 @@ export default function Home() {
     setItems(itemsJson)
   }, [])
 
+  function handleAdd(){
+    if(inputField === ""){
+      alert("You did not put any text in")
+    }
+    else{
+      let newItems = [...items,{content: inputField, checked: false, id: items.length + 1}]
+      setItems(newItems)
+      localStorage.setItem("items",JSON.stringify(newItems))
+      setInputField("")
+    }
+  }
+
   return (
     <div className="flex w-full">
       <div className="flex flex-col mx-auto mt-10">
           <h1 className="text-4xl mb-5">To-Do List</h1>
           <div className="flex flex-col gap-1">
+            <div>
+              <input type="text" className="text-black" value={inputField} onChange={(e)=>{setInputField(e.currentTarget.value)}}/>
+              <button className="bg-green-500 px-3 mx-2 mb-5" onClick={handleAdd}>+</button>
+            </div>
             {items.map((item)=>{
               return <Item setItems={setItems} details={item}/>
             })}
